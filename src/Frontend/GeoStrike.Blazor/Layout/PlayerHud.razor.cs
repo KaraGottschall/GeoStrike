a83cb528e556839b401e4d785007306ee33897c4
KaraGottschall <hschall56@gmail.com>
2026-07-08 19:50:52 -0300

    Adiciona Enum de recursos e Hud superior na tela main do frontend

---
+++ src/Frontend/GeoStrike.Blazor/Layout/PlayerHud.razor.cs
@@ -0,0 +1,20 @@
+﻿#region
+
+using Microsoft.AspNetCore.Components;
+
+#endregion
+
+namespace GeoStrike.Blazor.Layout;
+
+public partial class PlayerHud : ComponentBase
+{
+    protected int creditos = 45000;
+    protected int energia = 80;
+    protected int maxEnergia = 100;
+
+    protected int petroleo = 15400;
+
+    [Inject] protected HttpClient Http { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync() { await Task.CompletedTask; }
+}
