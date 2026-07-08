6d48aa96a2a927f0c7c840756c36f5536d4fa984
KaraGottschall <hschall56@gmail.com>
2026-07-08 20:21:49 -0300

    feat(ui): reorganiza layout, atualiza NavMenu e adiciona PlayerHud" -m "Atualiza MainLayout, refatora NavMenu, adiciona PlayerHud e ajusta Program.cs, _Imports.razor e index.html; atualiza GeoStrike.Blazor.csproj

--- src/Frontend/GeoStrike.Blazor/Program.cs
+++ src/Frontend/GeoStrike.Blazor/Program.cs
@@ -3,6 +3,7 @@
 using GeoStrike.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 #endregion
 
@@ -14,5 +15,7 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new("https://localhost:7200/")
 });
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
